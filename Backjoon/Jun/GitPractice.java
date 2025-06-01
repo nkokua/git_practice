51d900d07b746a285509f8c2f374ba83a0cbb7b5
nkokua <zy013@naver.com>
2025-06-01 14:42:14 +0900

    한줄 지움움

--- Backjoon/Jun/GitPractice.java
+++ Backjoon/Jun/GitPractice.java
@@ -3,5 +3,6 @@ package BackJoon.Jun;
 public class GitPractice {
     public static void main(String[] args) {
         System.out.println("Hello World git diff");
+        System.out.println("add");
     }
 }
